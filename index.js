57272080999b714948a08bc9c47f342d4eba15f0
skalashynski <sergei.kalashynski@gmail.com>
2023-04-29 21:20:30 +0400

    frontend

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//Enable bosy parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/openai', require('./routes/openaiRoutes'))
 
 
